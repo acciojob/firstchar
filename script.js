91f7bdb492b6befcd7e2248cec304efe3df32065
Harsh Sharma <harshs.iitd@gmail.com>
2024-07-11 13:06:23 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,6 +3,6 @@ function firstChar(text) {
 }
 
 // Do not change the code below
-
-const text = prompt("Enter text:");
+//Uncomment the following line to show the prompt popup
+//const text = prompt("Enter text:");
 alert(firstChar(text));
